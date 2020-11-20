c5e66a50052fd13f5a8943bd65160659d0b41b79
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-11-20 19:31:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,5 +47,7 @@ function reduceToTotal(src, startingPoint = 0){
 
 //6.
 function reduceToAllTrue(src) {
-  for (let i = 0; i < src.length; i++)
+  for (let i = 0; i < src.length; i++){
+    if (!src[i]) return false
+  }
 }
